dce06e3e21285cc7e95415582823a1bac0e353c9
jakobpolegek <jakobpolegek@hotmail.com>
2021-06-04 16:06:14 +0200

    dodal build za deployment na netlify
    dodal build za deployment na netlify

---
+++ spletna/build/precache-manifest.0e0363b85ddf39068aba2e68d3f03b94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9f2e25ebea87910e548a15f91e6c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a75346937271cbf80ad",
+    "url": "/static/css/main.6f61e41e.chunk.css"
+  },
+  {
+    "revision": "4ab1fb30738d3c83a783",
+    "url": "/static/js/2.d67c4918.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.d67c4918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a75346937271cbf80ad",
+    "url": "/static/js/main.ac8102e2.chunk.js"
+  },
+  {
+    "revision": "12ffa9c2a2e2bca05bc1",
+    "url": "/static/js/runtime-main.a41b20c0.js"
+  }
+]);
