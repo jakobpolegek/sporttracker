3ec97d5f5b3d82ce1e4fec9a32e953489d453394
jakobpolegek <jakobpolegek@hotmail.com>
2021-06-09 13:05:30 +0200

    Flask server
    Pripravu flask server za hostat

---
+++ PythonWeb/docker/firebase.py
@@ -0,0 +1,17 @@
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyBrNS9Y9zKUH3CESiD-Ab5Qw-0O8_Gy47g",
+    "authDomain": "sporttracker-fd60b.firebaseapp.com",
+    "databaseURL": "https://sporttracker-fd60b-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "sporttracker-fd60b",
+    "storageBucket": "sporttracker-fd60b.appspot.com",
+    "messagingSenderId": "978482245151",
+    "appId": "1:978482245151:web:840c67794c261b6d346f2a",
+    "serviceAccount": "serviceAccountKey.json"
+}
+
+firebase_storage = pyrebase.initialize_app(config)
+storage = firebase_storage.storage()
+
+storage.child("videoiI2Q3uhH9iUeYCiL6Bpb.mp4").download("videoiI2Q3uhH9iUeYCiL6Bpb.mp4")
