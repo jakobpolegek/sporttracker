6b3891bb81997d10eaef5378b83cad4b3677c198
janvrtacnik <jan.vrtacnik2000@gmail.com>
2021-06-10 15:41:54 +0200

    Added request and cannot change username

--- SportTrackerAndroid/app/src/main/java/com/example/sporttracker/VideoActivity.java
+++ SportTrackerAndroid/app/src/main/java/com/example/sporttracker/VideoActivity.java
@@ -2,19 +2,17 @@ package com.example.sporttracker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -22,6 +20,11 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class VideoActivity extends AppCompatActivity {
 
@@ -68,6 +71,7 @@ public class VideoActivity extends AppCompatActivity {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                             // Upload succeeded
+                            new postThread().execute();
                             Toast.makeText(getApplicationContext(), "Upload Success...", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(VideoActivity.this,MainActivity.class));
                         }
@@ -93,5 +97,37 @@ public class VideoActivity extends AppCompatActivity {
 
 
         }
+    }
+
+    class postThread extends AsyncTask<Void, Void, Void> {
+
+        private Exception exception;
+
+        protected Void doInBackground(Void... params) {
+            try {
+                URL url = new URL("http://83.212.126.21:5000/processData");
+                HttpURLConnection http = (HttpURLConnection)url.openConnection();
+                http.setRequestMethod("POST");
+                http.setDoOutput(true);
+                http.setRequestProperty("Content-Type", "application/json");
+
+                String data = "{\"idDoc\":\""+ app.thisUser.getDocID() +"\"}";
+                System.out.println("IDDOC iz requesta: " + app.thisUser.getDocID());
+                byte[] out = data.getBytes(StandardCharsets.UTF_8);
+
+                OutputStream stream = http.getOutputStream();
+                stream.write(out);
+
+                System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+                http.disconnect();
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
+
+            return null;
+        }
+
+
     }
 }
